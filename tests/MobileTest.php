40698bded0f20d6029953550c1d4efdb8a999906
Alvaro Calvo <acalvo@pucp.pe>
2021-03-24 20:45:16 -0500

    [test-fix] Added provider mock and imported classes to the MobileTest class

--- tests/MobileTest.php
+++ tests/MobileTest.php
@@ -3,6 +3,9 @@
 namespace Tests;
 
 use Mockery as m;
+use App\Mobile;
+use App\Interfaces\CarrierInterface;
+
 use PHPUnit\Framework\TestCase;
 
 class MobileTest extends TestCase
@@ -11,6 +14,8 @@ class MobileTest extends TestCase
 	/** @test */
 	public function it_returns_null_when_name_empty()
 	{
+		$provider = m::mock(CarrierInterface::class);
+
 		$mobile = new Mobile($provider);
 
 		$this->assertNull($mobile->makeCallByName(''));
